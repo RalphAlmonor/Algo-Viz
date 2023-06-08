3cdda1f39740518d406d6b17ceea0504f568f798
Ralph Almonor <dev.ralph@Ralphs-MacBook-Air.local>
2023-06-08 10:52:05 -0400

    testing

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 // Reset
+// const newDiv = document.createElement('div');
 
 // Make bars appear on screen 
 const bars = document.querySelector(".bars");
-const newDiv = document.createElement('div');
+const val = document.querySelector('.nums');
 let answer = [];
 
 // Function that makes a random number
@@ -13,7 +14,7 @@ randomNum = (min, max) => {
 // Function to make multiple random numbers 
 range = () => {
     for (let i = 0; i < 20; i++) {
-        answer.push(randomNum(5, 40));
+        answer.push(randomNum(5, 60));
     }    
     return answer;
 }
@@ -22,7 +23,8 @@ range();
 // For loop to make new divs and sizes the bars 
 for (let index = 0; index < answer.length; index++) {
     const div = document.createElement("div");
-    div.style.height = `${answer[index]}` + `1px` 
+    div.style.height = `${answer[index]}` + `0px`;
+    div.id = (`${index}`);
     div.className = 'nums';
     div.innerHTML = answer[index];
     bars.appendChild(div);
@@ -44,23 +46,41 @@ selectionSort = (array) => {
         }        
         if (smallestNum !== i) {
             // Swapping elements 
-            let hold = array[i];
+            hold = array[i];
             array[i] = array[smallestNum];
             array[smallestNum] = hold;
         }
     }
     return array
 }
-console.log(selectionSort(answer));
 
-test = () => {
-    alert('hello')
+// Bars to move 
+moveBars = () => {
+
+    // storage = answer[0];
+    // answer[0]= answer[1];
+    // answer[1]= storage;
+
 }
 
+let firstdiv = document.getElementById("0");
+let seconddiv = document.getElementById("1");
+
+console.log(firstdiv);
+console.log(seconddiv);
+console.log();
+hold = firstdiv;
+firstdiv = seconddiv;
+seconddiv = hold;
+
+console.log(firstdiv);
+console.log(seconddiv);
 
 
+console.log(answer[0]);
 
 
+console.log(selectionSort(answer));
 
 
 
